0ee006107676dd3b6e5c126487f3525fce173aa5
bravosuper <141135285+bravosuper@users.noreply.github.com>
2023-09-04 18:58:27 +0530

    Update app.js

--- app.js
+++ app.js
@@ -7,51 +7,30 @@ const port = process.env.PORT || 3001;
 
 var click_urls_array = [];
 
-function uuid(){
-  var uuid = Math.random().toString(16).substr(2, 32);
-
-  // Convert the random number to a UUID format.
-  uuid = "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function(c) {
-    var r = uuid.substr(Math.floor(Math.random() * 16), 1);
-    return r;
-  });
-
-  return uuid;
-}
-
-// Middleware to parse JSON in the request body
-app.use(bodyParser.json({limit: '50mb'}));
-app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
-
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*'); // Replace '*' with your allowed origin(s)
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
 
-app.get("/receive", (req, res) => {
+app.get("/record", (req,res) => {
+  var ip = req.query.ip;
   var click_url = req.query.clkurl;
-  if(click_url != undefined){
-    click_urls_array.push(click_url);
-  }
-  res.type("application/json").send(JSON.stringify({"message" : "Received", "url" : `${click_url}`}));
-})
 
-app.get("/request", (req,res) => {
-  if(click_urls_array.length > 0){
-    var click_url = click_urls_array.shift();
-    res.type("application/json").send(JSON.stringify({"message" : "available", "url" : `${click_url}`}));
-  }else{  
-    res.type("application/json").send(JSON.stringify({"message" : "not_available"}));
-  }
+  var obj = {ip, click_url};
+
+  click_urls_array.push(obj);
+
+  res.type("application/json").send(JSON.stringify(obj));
 })
 
 app.get("/getall", (req,res) => {
-  res.type("application/json").send(JSON.stringify({"urls_array" : `${JSON.stringify(click_urls_array)}`}));
+  res.type("application/json").send(JSON.stringify(click_urls_array));
 })
 
-app.get("/pinging", (req,res) => {
-  res.type("application/json").send(JSON.stringify({"message" : "Hello World"}));
+app.get("/delete", (req,res) => {
+  click_urls_array = [];
+  res.type("application/json").send(JSON.stringify(click_urls_array));
 })
 
 var server = app.listen(port, () => {
